060bb9d00b8e0c46474ee8b6f61ec75c2cca98a3
UtpalMaji <utpalmaji.um@gmail.com>
2020-10-13 04:54:17 +0530

    build: add password field

--- src/components/sign-up/SignUp.vue
+++ src/components/sign-up/SignUp.vue
@@ -3,42 +3,92 @@
     <div class="sign-up-page">
       <v-layout row wrap>
         <v-flex xs12 sm12 md12 lg12 xl12 class="main-container">
-          <v-layout row wrap >
+          <v-layout row wrap>
             <v-flex xs12 sm12 md7 lg7 xl7>
               <div class="ma-5 mx-7 px-3 main-module">
-                  <v-layout row wrap>
-                      <v-flex xs12 sm12 md12>
-                          <div class="my-2 mx-1">
-                              <div class="mb-1"><b class="blue--text heading">Google</b></div>
-                              <div class="mb-3"><b class="heading">Create your Google Account</b></div>
-                          </div>
-                      </v-flex>
-                      <v-flex xs12 sm12 md6 lg6 class="input">
-                          <v-text-field outlined dense label="First Name" :rules="rulesForName"></v-text-field>
-                      </v-flex>
-                      <v-flex xs12 sm12 md6 lg6 class="input">
-                          <v-text-field outlined dense label="Last Name" :rules="rulesForName"></v-text-field>
-                      </v-flex>
-                      <v-flex xs12 sm12 md12 lg12 class="input">
-                          <v-text-field outlined dense label="UserName" :rules="rulesForEmail"></v-text-field>
-                      </v-flex>
-                      <v-flex xs12 sm12 md12 lg12 class="input">
-                      </v-flex>
-                  </v-layout>
+                <v-layout row wrap>
+                  <v-flex xs12 sm12 md12>
+                    <div class="my-2 mx-1">
+                      <div class="mb-1">
+                        <b class="blue--text heading">Google</b>
+                      </div>
+                      <div class="mb-3">
+                        <b class="heading">Create your Google Account</b>
+                      </div>
+                    </div>
+                  </v-flex>
+                  <v-flex xs12 sm12 md6 lg6 class="input">
+                    <v-text-field
+                      outlined
+                      dense
+                      label="First Name"
+                      :rules="rulesForName"
+                    ></v-text-field>
+                  </v-flex>
+                  <v-flex xs12 sm12 md6 lg6 class="input">
+                    <v-text-field
+                      outlined
+                      dense
+                      label="Last Name"
+                      :rules="rulesForName"
+                    ></v-text-field>
+                  </v-flex>
+                  <v-flex xs12 sm12 md12 lg12 class="input">
+                    <v-text-field
+                      outlined
+                      dense
+                      label="UserName"
+                      hint="You can use letters, numbers & periods"
+                      suffix="@gmail.com"
+                      v-model="email"
+                      :rules="rulesForEmail"
+                    ></v-text-field>
+                  </v-flex>
+                  <v-flex xs12 sm12 md6 lg6 class="input">
+                    <v-text-field
+                      outlined
+                      dense
+                      v-model="password"
+                      :rules="rulesForPass"
+                      :type="passwordShow ? 'text' : 'password'"
+                      label="Password"
+                      @click:append="passwordShow = !passwordShow"
+                    ></v-text-field>
+                  </v-flex>
+                  <v-flex xs12 sm12 md6 lg6 class="input">
+                    <v-text-field
+                      outlined
+                      dense
+                      v-model="confirmPassword"
+                      :append-icon="passwordShow ? 'mdi-eye' : 'mdi-eye-off'"
+                      :rules="rulesForPass"
+                      :type="passwordShow ? 'text' : 'password'"
+                      label="Confirm Password"
+                      @click:append="passwordShow = !passwordShow"
+                    ></v-text-field>
+                  </v-flex>
+                  <v-flex xs12 sm12 md12 lg12 class="pl-1">
+                      <span style="font-size:13px">Use 8 or more characters with a mix of letters, numbers & symbols</span>
+                  </v-flex>
+                </v-layout>
               </div>
             </v-flex>
 
             <v-flex md5 lg5 xl5 class="pa-2 my-10 sconndery-module">
-                <v-flex md0 >
-                    <img src="../../assets/account.png" alt="" class="account-image">
-                    <span>One account. All of Google working for you.</span>
-                </v-flex>
+              <v-flex md0>
+                <img
+                  src="../../assets/account.png"
+                  alt=""
+                  class="account-image"
+                />
+                <span>One account. All of Google working for you.</span>
+              </v-flex>
             </v-flex>
           </v-layout>
         </v-flex>
 
         <v-flex xs12 sm12 md12 lg12 xl12 class="mt-3 footer">
-          <div>footer elements</div>
+            <span>footer</span>
         </v-flex>
       </v-layout>
     </div>
@@ -51,20 +101,36 @@ export default {
   components: {},
   data() {
     return {
-        rulesForName: [
-        value => !!value || 'Required.',
-        value => (value || '').length >= 3 || 'Min 3 characters',
-        value => {
+      email: "",
+      password: '',
+      confirmPassword: '',
+      passwordShow: false,
+      rulesForName: [
+        (value) => !!value || "Required.",
+        (value) => (value || "").length >= 3 || "Min 3 characters",
+        (value) => {
           const pattern = /^[A-Z]{1}[a-z]{2,}$/;
-          return pattern.test(value) || 'Invalid name.';
+          return pattern.test(value) || "Invalid name.";
         },
       ],
       rulesForEmail: [
-        value => !!value || 'Required.',
-        value => (value || '').length >= 8 || 'Min 8 characters',
-        value => {
-          const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-          return pattern.test(value) || 'Invalid e-main'
+        (value) => !!value || "Required.",
+        (value) => (value || "").length >= 8 || "Min 8 characters",
+        (value) => {
+          const pattern = /^[0-9a-z]+[+_.-]?[0-9a-z]/;
+          //const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+          return pattern.test(value) || "Invalid e-main";
+        },
+      ],
+      rulesForPass: [
+        (value) => !!value || "Required.",
+        (value) => (value || "").length >= 8 || "Min 8 characters",
+        (value) => {
+          const pattern = /^(?=.*[0-9])(?=.*[A-Z])(?=.*[*.!@$%^&(){}:;<>,?/~_+=|]).{8,}$/;
+          return pattern.test(value) || "Invalid e-main";
+        },
+        (value) => {
+          return this.password == (value) || "Invalid e-main";
         },
       ],
     };
@@ -80,19 +146,22 @@ export default {
   border: 1px solid #999;
   border-radius: 5px;
 }
-.main-module{ 
-    margin: inherit;
-    padding: inherit;
+.main-module {
+  margin: inherit;
+  padding: inherit;
 }
 .account-image {
-    height: 100%;
-    width: 98%;
+  height: 100%;
+  width: 98%;
 }
 .heading {
-    font-size: larger;
+  font-size: larger;
 }
 .input {
-    padding: 5px;
+  padding: 5px;
+}
+.v-text-field.v-text-field--enclosed .v-text-field__details {
+    margin-bottom: 0;
 }
 /* .footer {
 } */
