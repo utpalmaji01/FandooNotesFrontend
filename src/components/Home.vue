b4ed751dcd15ac6d1a86ee61b92d1bcb2769706c
UtpalMaji <utpalmaji.um@gmail.com>
2020-10-12 09:28:08 +0530

    Welcome to Fandoo Notes

---
+++ src/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container>
+    <div class="d-flex justify-center align-center ma-5 pa-5">
+      <div class="app">
+        Welcome to fondoo notes
+      </div>
+    </div>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Home",
+
+  data: () => ({}),
+};
+</script>
+<style scoped>
+.app{
+  width: 90%;
+  height: 90%;
+  border: 1px solid #999;
+  border-radius: 5px;
+}
+</style>
