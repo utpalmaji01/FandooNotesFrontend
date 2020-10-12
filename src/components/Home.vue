bb964a58b1f681b7b70c08df6ea02f44e9742dea
UtpalMaji <utpalmaji.um@gmail.com>
2020-10-12 09:58:30 +0530

    refactor: add component for modularity

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,25 +1,19 @@
 <template>
-  <v-container>
-    <div class="d-flex justify-center align-center ma-5 pa-5">
-      <div class="app">
-        Welcome to fondoo notes
-      </div>
-    </div>
-  </v-container>
+  <v-app>
+    <sign-up></sign-up>
+  </v-app>
 </template>
 
 <script>
+import SignUp from "./sign-up/SignUp";
 export default {
   name: "Home",
+  components: {
+    "sign-up": SignUp,
+  },
 
   data: () => ({}),
 };
 </script>
 <style scoped>
-.app{
-  width: 90%;
-  height: 90%;
-  border: 1px solid #999;
-  border-radius: 5px;
-}
 </style>
---
+++ src/components/sign-up/SignUp.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="d-flex justify-center align-center ma-5 pa-5">
+      <div class="app">Welcome to fondoo notes</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SignUp",
+  components: {},
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+.app {
+  width: 90%;
+  height: 90%;
+  border: 1px solid #999;
+  border-radius: 5px;
+}
+</style>
