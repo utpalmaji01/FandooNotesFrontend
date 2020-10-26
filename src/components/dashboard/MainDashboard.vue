23619181d1353631cdc949f227004ea1125252f4
UtpalMaji <utpalmaji.um@gmail.com>
2020-10-26 08:21:35 +0530

    add: side navbar of dashboard component

--- src/components/dashboard/MainDashboard.vue
+++ src/components/dashboard/MainDashboard.vue
@@ -1,6 +1,55 @@
 <template>
   <v-app>
     <app-header></app-header>
+    <v-layout row wrap>
+      <v-flex xs2>
+        <div class="ma-3">
+          <v-navigation-drawer permanent expand-on-hover>
+            <v-list>
+
+              <v-list-item link>
+                <v-list-item-icon>
+                  <v-icon>mdi-lightbulb-outline</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title>Notes</v-list-item-title>
+              </v-list-item>
+
+              <v-list-item link>
+                <v-list-item-icon>
+                  <v-icon>mdi-bell-outline</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title>Reminders</v-list-item-title>
+              </v-list-item>
+
+              <v-list-item link>
+                <v-list-item-icon>
+                  <v-icon>mdi-label-outline</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title>Labels</v-list-item-title>
+              </v-list-item>
+
+              <v-list-item link>
+                <v-list-item-icon>
+                  <v-icon>mdi-package-down</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title>Archive</v-list-item-title>
+              </v-list-item>
+
+              <v-list-item link>
+                <v-list-item-icon>
+                  <v-icon>mdi-delete-outline</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title>Trash</v-list-item-title>
+              </v-list-item>
+
+            </v-list>
+          </v-navigation-drawer>
+        </div>
+      </v-flex>
+      <v-flex xs10>
+        <p>maji</p>
+      </v-flex>
+    </v-layout>
   </v-app>
 </template>
 
